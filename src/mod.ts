a4af4b91cfccd80674b10f3f890b1411a7b520b6
Huan LI (李卓桓) <zixia@zixia.net>
2020-07-03 02:05:00 +0800

    export Vorpal

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,12 @@
+import Vorpal from 'vorpal'
+
 export { VERSION }        from './version'
 export {
   WechatyVorpalConfig,
   WechatyVorpal,
   WechatyVorpalMessages,
 }                         from './wechaty-vorpal'
+
+export {
+  Vorpal,
+}
